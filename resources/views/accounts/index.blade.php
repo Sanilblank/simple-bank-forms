aaed96533667f40315a54bbda212579a6eee9df9
Sanil Manandhar <sanilmanandhar@gmail.com>
2025-02-20 18:35:42 +0545

    Minor updates again

--- resources/views/accounts/index.blade.php
+++ resources/views/accounts/index.blade.php
@@ -1,12 +1,15 @@
 @extends('adminlte::page')
 
+@section('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+@endsection
 @section('content')
     <div class="container">
         @include('layouts.response')
         <h2>Accounts List</h2>
 
         @if (count($accounts))
-            <table class="table table-bordered">
+            <table class="table table-bordered datatable">
                 <thead>
                 <tr>
                     <th>Account ID</th>
@@ -48,4 +51,8 @@
             <p>No accounts found.</p>
         @endif
     </div>
+@endsection
+@section('js')
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('js/datatables.js') }}"></script>
 @endsection
